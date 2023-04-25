5f219240c27927756cefe04d00bfeb95f7883e17
Mayardes Oliveira <mayardesoliveira@gmail.com>
2023-04-24 23:52:51 -0300

    adding controller School, requests examples of School, removing WeatherForecastController and WeatherForecast view examples.

---
+++ SchoolAdersonDeMenezes.Api/Controllers/v1/RequestExamples/SchoolPostRequestExample.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using SchoolAdersonDeMenezes.Application.Commands;
+using Swashbuckle.AspNetCore.Filters;
+using SchoolAdersonDeMenezes.Application.Dtos.InputModels;
+
+namespace SchoolAdersonDeMenezes.Api.Controllers.v1.RequestExamples
+{
+    public class SchoolPostRequestExample : IMultipleExamplesProvider<AddSchoolCommand>
+    {
+        public IEnumerable<SwaggerExample<AddSchoolCommand>> GetExamples()
+        {
+            var faker = new Faker("pt_BR");
+            Guid id = Guid.NewGuid();
+
+            yield return new SwaggerExample<AddSchoolCommand>
+            {
+                Name = "Example valid #1",
+                Summary = "All filled",
+                Value = new AddSchoolCommand()
+                {
+                    Name = faker.Name.FullName(),
+                    Address = new AddressInputModel(faker.Address.StreetName(), faker.Address.City(), faker.Address.State(), faker.Address.StreetSuffix()),
+                    Students = new List<StudentInputModel>()
+                    {
+                       new StudentInputModel(id, faker.Person.FullName, faker.Person.Email, new ParentsInputModel(faker.Random.Guid(), id, faker.Person.FullName, faker.Person.Email, Domain.Enums.Parent.Father)),
+                       
+                    },
+                    TypePublicEducation = Domain.Enums.TypePublicEducation.Municipal
+                }
+            };
+        }
+    }
+}
