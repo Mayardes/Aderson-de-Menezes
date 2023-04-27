33884e6deac4ccf6a224a3014673baee060504a5
Mayardes Oliveira <mayardesoliveira@gmail.com>
2023-04-27 00:04:24 -0300

    adding ToDashCase for create nomenclature of routingKey as standard and calling service message in AddSchoolHandler

--- SchoolAdersonDeMenezes.Application/Commands/Handlers/AddSchoolHandler.cs
+++ SchoolAdersonDeMenezes.Application/Commands/Handlers/AddSchoolHandler.cs
@@ -1,19 +1,32 @@
 ﻿using MediatR;
+using SchoolAdersonDeMenezes.Domain.Entities;
 using SchoolAdersonDeMenezes.Domain.Repositories;
+using SchoolAdersonDeMenezes.Infraestructure.MessageBus;
 
 namespace SchoolAdersonDeMenezes.Application.Commands.Handlers
 {
     public class AddSchoolHandler : IRequestHandler<AddSchoolCommand, Guid>
     {
         private readonly ISchoolRepository _repository;
-        public AddSchoolHandler(ISchoolRepository repository)
+        private readonly IMessageBusClient _messageBus;
+        public AddSchoolHandler(ISchoolRepository repository, IMessageBusClient messageBus)
         {
             _repository = repository;
+            _messageBus = messageBus;
         }
         public async Task<Guid> Handle(AddSchoolCommand request, CancellationToken cancellationToken)
         {
             var school = request.ToEntity();
-            return await _repository.AddAsync(school);
+            await _repository.AddAsync(school);
+
+            foreach (var @event in school.Events)
+            {
+              var routingKey = @event.GetType().Name.ToDashCase();
+
+              _messageBus.Publish(@event, routingKey, "school-service");
+            }
+
+            return school.Id;
         }
     }
 }
--- SchoolAdersonDeMenezes.Application/Extensions.cs
+++ SchoolAdersonDeMenezes.Application/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using System.Text;
 
 namespace SchoolAdersonDeMenezes.Application
 {
@@ -10,6 +11,31 @@ namespace SchoolAdersonDeMenezes.Application
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
             return services;
+        }
+
+        public static string ToDashCase(this string text)
+        {
+            if(text == null)
+                throw new ArgumentException(nameof(text));
+            if(text.Length < 2)
+                return text;
+            var sb = new StringBuilder();
+            sb.Append(char.ToLowerInvariant(text[0]));
+            for(int i=1; i<text.Length; i++)
+            {
+                char c = text[i];
+                if (char.IsUpper(c))
+                {
+                    sb.Append('-');
+                    sb.Append(char.ToLowerInvariant(c));
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
